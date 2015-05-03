616ca74f877eedd58ead96cbd77fc7f925439d4e
KureFM <kure.fukumakoto@gmail.com>
2015-05-03 19:32:21 +0800

    修改Login类的结构

--- src/crawler.py
+++ src/crawler.py
@@ -11,10 +11,90 @@ import re
 import binascii
 import os
 from packages import rsa
+from exceptions import *
 
 
+class HttpOperation(object):
+    def __init__(self, cookie_filename=None, cookie_type='Mozilla', default_headers=None):
+        """
+        Initialize default http header and cookie
+        :param cookie_filename:
+        :param cookie_type:
+        :param default_headers:
+        """
+        self._default_headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36',
+            'Accept-Language': 'zh-CN'
+        } if not default_headers else default_headers
+        self._cookie = None
 
-class Login(object):
+        # cookie保存文件
+        if cookie_filename:
+            self._cookie_filename = cookie_filename
+
+            # 创建cookie，带保存文件
+            if cookie_type.lower() == 'mozilla':
+                self._cookie = cookielib.MozillaCookieJar(self._cookie_filename)
+            elif cookie_type.lower() == 'lwp':
+                self._cookie = cookielib.LWPCookieJar(self._cookie_filename)
+            else:
+                raise CookieTypeError("Unsupported cookie type: %s" % cookie_type)
+        else:
+            self._cookie = cookielib.CookieJar()
+
+
+        # 用cookiejar创建build_opener
+        _opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self._cookie))
+        # 将opener装入urllib2
+        urllib2.install_opener(_opener)
+
+
+    def __get_response(self, url, params={}, headers={}):
+        data = None
+
+        # 对参数编码
+        if params:
+            data = urllib.urlencode(params)
+        # 创建Request对象
+        request = urllib2.Request(url, data, headers, )
+        # 添加默认Header
+        for (key, value) in self._default_headers.items():
+            if not request.headers.has_key(key):
+                request.headers[key] = value
+        # 打开url连接
+        return urllib2.urlopen(request)
+
+    def get(self, url, params={}, headers={}):
+        """
+        Use get method to request page
+        :param url:witch url do you want to request
+        :param params:append params in request
+        :param headers:append headers in request
+        :return:it with return a response object
+        """
+        if params:
+            url = '{url}?{encoded_params}'. \
+                format(url=url, encoded_params=urllib.urlencode(params))
+        return self.__get_response(url, headers=headers)
+
+    def post(self, url, params={}, headers={}):
+        """
+        Use post method to request page
+        :param url:witch url do you want to request
+        :param params:append params in request
+        :param headers:append headers in request
+        :return:it with return a response object
+        """
+        return self.__get_response(url, params, headers)
+
+    def load_cookie(self, filename=None, ignore_discard=False, ignore_expires=False):
+        self._cookie.load(filename, ignore_discard, ignore_expires)
+
+    def save_cookie(self, filename=None, ignore_discard=False, ignore_expires=False):
+        self._cookie.save(filename, ignore_discard, ignore_expires)
+
+
+class Login(HttpOperation):
     """The class use to login weibo"""
     _CLIENT = 'ssologin.js(v1.4.15)'
     _LOGIN_ROOT_URL = 'http://login.sina.com.cn/'
@@ -52,48 +132,17 @@ class Login(object):
             'returntype': 'IFRAME',
             'setdomain': '1'
         }
-        self.__default_headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36',
-            'Accept-Language': 'zh-CN'
-        }
+        super(Login, self).__init__(os.path.join('cookies','[%s].cookie' % username))
 
-        # cookie保存文件
-        self.__cookieFile = os.path.join('cookies', '[{username}].cookie'.format(username=username))
-        # 创建cookie，带保存文件
-        self.__cookie = cookielib.MozillaCookieJar(self.__cookieFile)
-        # 用cookiejar创建build_opener
-        opener = urllib2.build_opener(
-            urllib2.HTTPCookieProcessor(self.__cookie))
-        # 将opener装入urllib2
-        urllib2.install_opener(opener)
-
-        # 从文件中加载cookie
         try:
-            self.__cookie.load()
-        # 文件不存在
+            self.load_cookie()
         except IOError as ioe:
             print 'cookie not exist'
             self.relogin(username, password)
-        # 文件存在，检测cookie可用性
         else:
             if self.check_login_state():
                 self.relogin(username, password)
             print 'cookie is ok'
-
-    def __get_response(self, url, params={}, headers={}):
-        data = None
-
-        # 对参数编码
-        if params:
-            data = urllib.urlencode(params)
-        # 创建Request对象
-        request = urllib2.Request(url, data, headers)
-        # 添加默认Header
-        for (key, value) in self.__default_headers.items():
-            if not request.headers.has_key(key):
-                request.headers[key] = value
-        # 打开url连接
-        return urllib2.urlopen(request)
 
     def __prelogin(self, username, password):
         """
@@ -131,7 +180,7 @@ class Login(object):
         for url in urls:
             self.get(url[0].replace('\\', ''))
         # 保存cookie
-        self.__cookie.save()
+        self.save_cookie()
 
     def check_login_state(self):
         """
@@ -145,33 +194,9 @@ class Login(object):
         else:
             return False
 
-    def get(self, url, params={}, headers={}):
-        """
-        Use get method to request page
-        :param url:witch url do you want to request
-        :param params:append params in request
-        :param headers:append headers in request
-        :return:it with return a response object
-        """
-        if params:
-            url = '{url}?{encoded_params}'. \
-                format(url=url, encoded_params=urllib.urlencode(params))
-        return self.__get_response(url, headers=headers)
-
-    def post(self, url, params={}, headers={}):
-        """
-        Use post method to request page
-        :param url:witch url do you want to request
-        :param params:append params in request
-        :param headers:append headers in request
-        :return:it with return a response object
-        """
-        return self.__get_response(url, params, headers)
-
-
 if __name__ == '__main__':
     account = None
     with open('accounts', 'rb') as f:
         account = f.readline().split(',')
     if account:
-        l = Login(account[0],account[1])
+        l = Login(account[0], account[1])
---
+++ src/exceptions.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# module exceptions
+
+class CookieTypeError(TypeError):
+    """An cookie type error occurred."""
